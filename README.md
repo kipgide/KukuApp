06f287c7ac67d101f79329ec3993c24e6b49d639
Gideon <kiplagatgideon@gmail.com>
2016-07-15 18:10:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kuku-App
+
+Manage chicken\\
+Gideon Kiplagat
+S13/2O870/12
+Assignment
---
+++ src/appinventor/ai_gideonk/Gideon_Kiplagat/Breeding.scm
@@ -0,0 +1,37 @@
+#|
+$JSON
+{"authURL":["*UNKNOWN*","ai2.appinventor.mit.edu"],"YaVersion":"157","Source":"Form","Properties":
+{"$Name":"Breeding","$Type":"Form","$Version":"19","AppName":"Gideon_Kiplagat","Scrollable":"True",
+"Sizing":"Responsive","Title":"Breeding Tips!","Uuid":"0","$Components":[{"$Name":"HorizontalArrangement1",
+"$Type":"HorizontalArrangement","$Version":"3","Width":"-2","Uuid":"1681616377","$Components":[{"$Name":"Label1",
+"$Type":"Label","$Version":"4","BackgroundColor":"&HFF00FF00","FontSize":"30","HasMargins":"False","Width":"-2","Text"
+:"How to Breed Chickens","TextAlignment":"1","Uuid":"766544051"}]},{"$Name":"VerticalArrangement1","$Type":"VerticalArrangement
+","$Version":"3","Width":"-2","Uuid":"-2039446823","$Components":[{"$Name":"HorizontalArrangement2","$Type":"HorizontalArrangement
+","$Version":"3","Width":"-2","Uuid":"1864384428","$Components":[{"$Name":"VerticalArrangement2","$Type":"VerticalArrangement","$Vers
+ion":"3","Width":"-2","Uuid":"1999771623","$Components":[{"$Name":"HorizontalArrangement6","$Type":"HorizontalArrangement","$Version":"3",
+"Width":"-2","Uuid":"-2139446041","$Components":[{"$Name":"Label2","$Type":"Label","$Version":"4","FontSize":"20","HasMargins":"False","Width
+":"-2","Text":" Method 1 Preparing to Breed ","Uuid":"-378845261"}]},{"$Name":"VerticalArrangement3","$Type":"VerticalArrangement","$Version":
+"3","Width":"-2","Uuid":"1729194301","$Components":[{"$Name":"HorizontalArrangement7","$Type":"HorizontalArrangement","$Version":"3","Width":"-2"
+,"Uuid":"180287150","$Components":[{"$Name":"Label6","$Type":"Label","$Version":"4","FontSize":"16","HasMargins":"False","Width":"-2","Text":"1 
+Find out if it is legal to keep chickens in your area. Some places have strict laws on keeping chickens whilst other places has ruled on keeping
+ roosters and the amount of chickens you have. To avoid a fine it is best to check your local laws and regulations in your area.","Uuid":"824537806"}
+ ]},{"$Name":"HorizontalArrangement3","$Type":"HorizontalArrangement","$Version":"3","Width":"-2","Uuid":"-53055708","$Components":[{"$Name":"Label3",
+ "$Type":"Label","$Version":"4","FontSize":"16","HasMargins":"False","Width":"-2","Text":"2 Make sure that you can house the new chickens. Many people
+ don\u2019t take into account that when you breed chickens, you will end up with significantly more chickens than you started with. Ensure that your 
+ chicken coop is ready for the new additions to your flock.","Uuid":"-1704472929"}]},{"$Name":"HorizontalArrangement4","$Type":"HorizontalArrangement",
+ "$Version":"3","Width":"-2","Uuid":"-1079395472","$Components":[{"$Name":"Label4","$Type":"Label","$Version":"4","FontSize":"16","HasMargins":"False"
+ ,"Width":"-2","Text":"3 Decide if you want to take a chance on more roosters. Breeding chickens will result in about 50% male chicks. Male chickens w
+ 
+ ill not contribute to your egg production, eat larger amounts of food, and will significantly increase the noise level of your flock. Be aware that b
+ reeding chickens will always result in having to deal with roosters.","Uuid":"-1223273549"}]},{"$Name":"HorizontalArrangement5","$Type":"HorizontalAr
+ rangement","$Version":"3","Width":"-2","Uuid":"1642310461","$Components":[{"$Name":"Label5","$Type":"Label","$Version":"4","FontSize":"16","HasMargins":"F
+ alse","Width":"-2","Text":"4 Obtain a rooster. In order to fertilize your hen\u2019s eggs, you will need a rooster in prime breeding condition. The rooste
+ r does not need to be the same breed as your hens in order to mate.[1] You should have 1 rooster for every 10 hens.","Uuid":"-813698917"}]},{"$Name":"Horizo
+ ntalArrangement8","$Type":"HorizontalArrangement","$Version":"3","Width":"-2","Uuid":"1145065803","$Components":[{"$Name":"Button1","$Type":"Button","$Vers
+ ion":"6","Text":"Read more...","Uuid":"-802813704"}]},{"$Name":"HorizontalArrangement9","$Type":"HorizontalArrangement","$Version":"3","AlignHorizontal":"2",
+ "Width":"-2","Uuid":"-195013746","$Components":[{"$Name":"Button2","$Type":"Button","$Version":"6","Height":"50","Width":"120","Image":"back_button1.png","
+ Uu
+ id":"11
+ 04944473"}]}]}]}]}]},{"$Name":"ActivityStarter1","$Type":"ActivityStarter","$Version":"6","Action":"android.intent.action.VIEW","DataUri":"http:\/\/www.wik
+ ihow.com\/Breed-Chickens","Uuid":"2053510143"}]}}
+|#
